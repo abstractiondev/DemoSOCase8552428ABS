fedd7b334dd420ae5e9edfeccf0f78b9e6661720
Kalle Launiala <kalle.launiala@protonit.net>
2011-12-23 00:11:59 +0200

    Added demo structure

---
+++ DemoSOCase8552428/ExampleBusinessGenerator_v1_0.designer.cs
@@ -0,0 +1,17 @@
+﻿ 
+
+		// This should be C# or something else (change the extension definitions...)
+		// For solution called: nameofSolution
+				
+		// Here starts project stuff for the project
+		// This is project stuff for project name of Project
+		
+				
+		// Here starts project stuff for the project
+		// This is project stuff for project name of Project
+		
+				
+		// Here starts project stuff for the project
+		// This is project stuff for project name of Project
+		
+		
